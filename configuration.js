7c9e1d6e98a5028e14267c1de78049e343c5271b
brilord <95247252+Brilord@users.noreply.github.com>
2024-05-18 19:21:37 -0400

    committing

--- configuration.js
+++ configuration.js
@@ -81,32 +81,7 @@ async function signup() {
         alert("Something went wrong!");
       }    
 }
-async function displayTheQuizzes() {
-    if (quizzes.length == 0) {
-      quizzes = await getAllQuizzes();
-    }
 
-    let list = document.getElementById("list");
-    list.innerHTML = "";
-
-    for (let quiz of quizzes) {
-      let div = document.createElement("div");
-      div.id = `{quiz.id}`;
-      div.className = "card";
-      div.onclick = function () {
-          location.href = `quiz.html?id=${quiz.id}`;
-      };
-      let innerHtml = `           
-    <div class="container">
-      <h4><b>${quiz.id}</b></h4> 
-      <p>${quiz.title}</p>   
-      <p>(${quiz.questionIds.length} questions)</p>           
-    </div>       
-        `;
-      div.innerHTML = innerHtml;
-      list.appendChild(div);
-    }
-}
 
 async function addBlog() {
     
@@ -155,7 +130,6 @@ async function saveTheQuiz() {
 
 
 async function login() {  
-      
     let username = document.getElementById("signin-username").value;
     let password = document.getElementById("signin-password").value;
     console.log(username + " " + password);
